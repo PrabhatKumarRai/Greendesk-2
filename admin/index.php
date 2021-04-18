afa511a6baa76a1b49eb081716f95b0bf11a634d
Prabhat Kumar Rai <69166399+PrabhatKumarRai@users.noreply.github.com>
2021-04-18 12:49:20 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Postman - Admin Login</title>
+    <title>Greendesk - Admin Login</title>
     <link rel="icon" href="../uploads/app/logo.png" type="image/gif" sizes="16x16">
 
     <!-- Dependencies Section Starts -->
